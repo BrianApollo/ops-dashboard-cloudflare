0ee79bb28b2b384aae62013728e4ecd5bcd5c628
brian <htetlinnoo19@gmail.com>
2026-07-20 11:09:37 +0700

    re-adjust add ads button

--- src/pages/campaigns/CampaignViewPage.tsx
+++ src/pages/campaigns/CampaignViewPage.tsx
@@ -1318,6 +1318,7 @@ function ManageTab({ campaign, fbData, accessToken, adAccountId }: ManageTabProp
                         alert('No template ad found in this ad set');
                       }
                     }}
+                    disabled={adSetAds.length === 0}
                   >
                     Add Ads
                   </Button>
